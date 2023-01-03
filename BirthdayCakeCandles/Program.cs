c8c0f2fd3529e23a1942de355ea4d4848b9c1bc6
Serpil81 <44366185+Serpil81@users.noreply.github.com>
2023-01-04 01:55:04 +0300

    Initial Commit

---
+++ BirthdayCakeCandles/Program.cs
@@ -0,0 +1,54 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'birthdayCakeCandles' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts INTEGER_ARRAY candles as parameter.
+     */
+
+    public static int birthdayCakeCandles(List<int> candles)
+    {
+        candles.Sort();
+        int index=0;
+        foreach (var item in candles)
+        {
+            if(item==candles[candles.Count-1] )
+            index=(candles.IndexOf(item));  
+        }
+return ((candles.Count)-index);
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+       
+
+        
+
+        List<int> candles = new List<int>{3,2,1,3};
+        
+
+        int result = Result.birthdayCakeCandles(candles);
+
+        System.Console.WriteLine(result);
+    }
+}
--- MinMaxSum/Program.cs
+++ MinMaxSum/Program.cs
@@ -1,2 +1,51 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'miniMaxSum' function below.
+     *
+     * The function accepts INTEGER_ARRAY arr as parameter.
+     */
+
+    public static void miniMaxSum(List<int> arr)
+    {
+        long min = 0;
+        long max = 0;
+        long sum = 0;
+        foreach (var item in arr)
+        {
+            sum += item;
+        }
+        arr.Sort();
+        min = sum - arr[arr.Count - 1];
+        max = sum - arr[0];
+
+        System.Console.WriteLine(min + " " + max);
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+
+        List<int> arr = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp)).ToList();
+
+        Result.miniMaxSum(arr);
+    }
+}
