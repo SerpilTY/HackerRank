e0ab7c20240ec4eee4d6141fcce40fe1142793c8
Serpil81 <44366185+Serpil81@users.noreply.github.com>
2023-01-03 22:16:44 +0300

    We do that sport

--- PlusMinus/Program.cs
+++ PlusMinus/Program.cs
@@ -23,26 +23,25 @@ class Result
 
     public static void plusMinus(List<int> arr)
     {
-        List<double> result=new List<double>();
-        double positive=0.0; result.Add(positive);
-        double negative=0.0; result.Add(negative);
-        double zero=0.0; result.Add(zero);
-        
-foreach (var item in arr)
-{
-    if(item>0){positive++;}
-    if(item<0){negative++;}
-    if(item==0){zero++;}
-    
-}
-    positive=positive/Convert.ToDouble(arr.Count);
-    negative=negative/Convert.ToDouble(arr.Count);
-    zero=zero/Convert.ToDouble(arr.Count);
-    System.Console.WriteLine(positive.ToString("F6"));
-    System.Console.WriteLine(negative.ToString("F6"));
-    System.Console.WriteLine(zero.ToString("F6"));
-    
-      
+        List<double> result = new List<double>();
+        double positive = 0.0; result.Add(positive);
+        double negative = 0.0; result.Add(negative);
+        double zero = 0.0; result.Add(zero);
+
+        foreach (var item in arr)
+        {
+            if (item > 0) { positive++; }
+            if (item < 0) { negative++; }
+            if (item == 0) { zero++; }
+
+        }
+        positive = positive / Convert.ToDouble(arr.Count);
+        negative = negative / Convert.ToDouble(arr.Count);
+        zero = zero / Convert.ToDouble(arr.Count);
+        System.Console.WriteLine(positive.ToString("F6"));
+        System.Console.WriteLine(negative.ToString("F6"));
+        System.Console.WriteLine(zero.ToString("F6"));
+
     }
 
 }
