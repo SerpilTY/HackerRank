2572f7ef568bc93ce3460ef265a624865c763c23
Serpil81 <44366185+Serpil81@users.noreply.github.com>
2023-01-06 13:45:29 +0300

    My* Commit

--- MigratoryBirds/Program.cs
+++ MigratoryBirds/Program.cs
@@ -18,18 +18,20 @@ class Result
     public static int migratoryBirds(List<int> arr)
     {
     double one = 0.1; double two = 0.2; double three = 0.3;double four = 0.4; double five = 0.5;
-        foreach (var item in arr)
-        {   if (item.Equals(1)) { one++; }
-            else if (item.Equals(2)) { two++; }
-            else if (item.Equals(3)) { three++; }
-            else if (item.Equals(4)) { four++; }
-            else if (item.Equals(5)) { five++; } }
+            foreach (var item in arr){
+          switch (item)
+        {   case 1: one++;   break;
+            case 2: two++;   break;
+            case 3: three++; break;
+            case 4: four++;  break;
+            case 5: five++;  break;
+        } }
 
         List<double> myArr = new List<double> {one, two, three, four, five};
         myArr.Sort();
 
-        double biggest = myArr[4];
-        int result = 0;
+        double biggest = myArr[4];  int result = 0;
+
         for (int i = 0; i < myArr.Count; i++){        
             if (Convert.ToInt32(myArr[i]) == Convert.ToInt32(biggest))
             {   result = Convert.ToInt32((myArr[i] - Math.Truncate(myArr[i])) * 10);
@@ -43,7 +45,7 @@ class Solution
 {
     public static void Main(string[] args)
     {
-        List<int> arr = new List<int> { 1, 4, 4, 4, 5, 3 };
+        List<int> arr = new List<int> { 1, 2, 3, 4, 5, 4, 3, 2, 1, 3, 4 };
         int result = Result.migratoryBirds(arr);
         System.Console.WriteLine(result);
     }
