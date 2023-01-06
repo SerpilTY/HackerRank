ac867bc28b9fc9d8d8ed2034ed485e54129cbb72
Serpil81 <44366185+Serpil81@users.noreply.github.com>
2023-01-06 13:37:05 +0300

    My* Commit

--- MigratoryBirds/Program.cs
+++ MigratoryBirds/Program.cs
@@ -17,49 +17,34 @@ class Result
 
     public static int migratoryBirds(List<int> arr)
     {
-        double one = 0.1; double two = 0.2; double three = 0.3;
-        double four = 0.4; double five = 0.5;
-        int result = 0;
-
+    double one = 0.1; double two = 0.2; double three = 0.3;double four = 0.4; double five = 0.5;
         foreach (var item in arr)
-        {
-            if (item.Equals(1)) { one++; }
+        {   if (item.Equals(1)) { one++; }
             else if (item.Equals(2)) { two++; }
             else if (item.Equals(3)) { three++; }
             else if (item.Equals(4)) { four++; }
-            else if (item.Equals(5)) { five++; }
-        }
+            else if (item.Equals(5)) { five++; } }
 
         List<double> myArr = new List<double> {one, two, three, four, five};
         myArr.Sort();
 
         double biggest = myArr[4];
-        for (int i = 0; i < myArr.Count; i++)
-        {
+        int result = 0;
+        for (int i = 0; i < myArr.Count; i++){        
             if (Convert.ToInt32(myArr[i]) == Convert.ToInt32(biggest))
-            {
-                result = Convert.ToInt32((myArr[i] - Math.Truncate(myArr[i])) * 10);
-                break;
-            }
+            {   result = Convert.ToInt32((myArr[i] - Math.Truncate(myArr[i])) * 10);
+                break; }
             else
-            {
-                result = Convert.ToInt32(myArr[4] - Math.Truncate(myArr[4]));
-            }
-        }
+            {   result = Convert.ToInt32(myArr[4] -Math.Truncate(myArr[4])); } }
         return result;
-    }
-}
+    } }
 
 class Solution
 {
     public static void Main(string[] args)
     {
-
-
-        List<int> arr = new List<int> { 1, 2, 3, 4, 5, 4, 3, 2, 1, 3, 4 };
-
+        List<int> arr = new List<int> { 1, 4, 4, 4, 5, 3 };
         int result = Result.migratoryBirds(arr);
-
         System.Console.WriteLine(result);
     }
 }
