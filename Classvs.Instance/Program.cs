d4035a2ebc032913b5497c8c758cd2d3fe644a04
Serpil81 <44366185+Serpil81@users.noreply.github.com>
2023-01-08 01:55:53 +0300

    Initial Commit

---
+++ Classvs.Instance/Program.cs
@@ -0,0 +1,45 @@
+﻿class Person
+{
+
+    public int age;
+
+    public Person(int initialAge)
+    {
+        if (initialAge >= 0) age = initialAge;
+        else if (initialAge < 0)
+        {
+            System.Console.WriteLine("Age is not valid, setting age to 0.");
+            initialAge = 0;
+            age = initialAge;
+        }
+    }
+
+    void yearPasses()
+    {
+        age++;
+    }
+
+    void amIOld()
+    {
+        if (age < 13) System.Console.WriteLine("You are young.");
+        else if (age >= 13 && age < 18) System.Console.WriteLine("You are a teenager..");
+        else System.Console.WriteLine("You are a old..");
+    }
+
+    static void Main(String[] args)
+    {
+        int T = int.Parse(Console.In.ReadLine());
+        for (int i = 0; i < T; i++)
+        {
+            int age = int.Parse(Console.In.ReadLine());
+            Person p = new Person(age);
+            p.amIOld();
+            for (int j = 0; j < 3; j++)
+            {
+                p.yearPasses();
+            }
+            p.amIOld();
+            Console.WriteLine();
+        }
+    }
+}
