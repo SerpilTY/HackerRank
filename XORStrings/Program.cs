d30cb43bbf6c995d8ae86df35f7751a6baf73bba
Serpil81 <44366185+Serpil81@users.noreply.github.com>
2023-01-17 17:40:05 +0300

    Initial Commit

---
+++ XORStrings/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Solution
+{
+    class Solution
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            string input2 = Console.ReadLine();
+            string res="";
+
+            for (int i = 0; i < input.Length; i++)
+                {
+                   res= (input[i] == input2[i]) ? res+="0" : res="1";              
+                }
+            
+            Console.WriteLine(res);
+        }
+    }
+}
