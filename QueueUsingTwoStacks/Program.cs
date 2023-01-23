aed5e6c69274e0b873bf670103228e612d2fc385
Serpil81 <44366185+Serpil81@users.noreply.github.com>
2023-01-24 00:53:57 +0300

    Initial Commit

---
+++ QueueUsingTwoStacks/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Dynamic;
+using System.Collections;
+using System;
+using System.Collections.Generic;
+using System.IO;
+class Solution {
+    static void Main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution 
+        */
+
+        Queue<int> myQueue=new Queue<int>();
+        string choice;
+        int q=Convert.ToInt32(Console.ReadLine());
+
+        for (int i = 1; i <= q; i++)
+        {
+            choice=(Console.ReadLine());
+            if(choice.StartsWith("1")){
+                string[] str=choice.Split(' ').ToArray();
+                int a=Convert.ToInt32(str[1]);
+                myQueue.Enqueue(a);
+            }
+            if(choice=="2"){
+                myQueue.Dequeue();
+            }
+            if(choice=="3"){
+                Console.WriteLine(myQueue.Peek()); 
+            }
+        }
+    }
+}
