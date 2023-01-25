54f93f9c5b54dcc7f4be22d53442f648a0c0fea5
Serpil81 <44366185+Serpil81@users.noreply.github.com>
2023-01-25 23:48:51 +0300

    Initial Commit

---
+++ BalancedBrackets/Program.cs
@@ -0,0 +1,86 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'isBalanced' function below.
+     *
+     * The function is expected to return a STRING.
+     * The function accepts STRING s as parameter.
+     */
+
+    public static string isBalanced(string s)
+    {
+        Stack<char> myStack = new Stack<char>();
+        char[] myChars = s.ToCharArray();
+        string result = "";
+
+        for (var k = 0; k < (myChars.Length) / 2; k++)
+        {
+            myStack.Push(myChars[k]);
+
+        }
+
+
+        if(myChars.Length%2==1) result="NO";  
+        else{
+
+        for (int i = (myChars.Length) / 2; i < (myChars.Length/ 2)+1; i++)
+        { 
+     
+            if (myChars[i] == ')' && myStack.First() == '(') {
+
+                 result = "YES"; }
+            else if (myChars[i] == ']' && myStack.First() == '[') {
+    
+                 result = "YES"; }
+            else if (myChars[i] == '}' && myStack.First() == '{') {
+      
+                 result = "YES"; } else { 
+           result = "NO"; break; }
+                    
+        for (int j = (myChars.Length) / 2; j < (myChars.Length); j++)
+        {
+            if (myChars[j] == ')' && myStack.Contains('(')) { result = "YES"; }
+            else if (myChars[j] == ']' && myStack.Contains('[')) { result = "YES"; }
+            else if (myChars[j] == '}' && myStack.Contains('{')) { result = "YES"; }
+            else if (myChars[j] == '(' && myStack.Contains(')')) { result = "YES"; }
+            else if (myChars[j] == '[' && myStack.Contains(']') ) { result = "YES"; }
+            else if (myChars[j] == '{' && myStack.Contains('}')) { result = "YES"; }
+
+            else { result = "NO"; break; }
+        }
+        }}
+        
+
+        return result;
+    }
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+
+        string s = "{(([])[])[]}";
+
+        string result = Result.isBalanced(s);
+
+        Console.WriteLine(result);
+    }
+
+}
+
