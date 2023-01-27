4c4822eb6d2018b02751a8b992cce9055f100e70
Serpil81 <44366185+Serpil81@users.noreply.github.com>
2023-01-27 16:01:05 +0300

    Initial Commit

--- SimpleTextEditor/Program.cs
+++ SimpleTextEditor/Program.cs
@@ -74,15 +74,15 @@ class Solution
                 }
                 else if
                  (queryNumbers[(queryNumbers.Count) - sequence] == 3)
-                {
-                    sequence++;
-                }
+                
+                    continue;
             }
         }
     }
 }
 
 /*
+Test Values
 1 abc
 3 3
 2 3
@@ -91,4 +91,84 @@ class Solution
 4
 4
 3 1
+
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+class Solution {
+    /*
+    1. append - Append string  to the end of .
+    2. delete - Delete the last  characters of .
+    3. print - Print the  character of .
+    4. undo - Undo the last (not previously undone) operation of type  or , reverting  to the state it was in prior to that operation.
+      
+    static void CheckCommand(ref Stack<Tuple<int, string>> stackCommands, Tuple<int, string> command, ref string text, bool isUndo = false) {
+        Tuple<int, string> undoCommand;
+        int k;
+    
+        switch (command.Item1) {
+            case 1:
+                string textToAppend = command.Item2;
+                undoCommand = Tuple.Create(2, textToAppend.Length.ToString());
+                              
+                if (!isUndo) {
+                    stackCommands.Push(undoCommand);
+                }
+
+                text += command.Item2;
+                break;
+            case 2:
+                k = Convert.ToInt32(command.Item2);
+                int deleteFrom = text.Length - k;
+                undoCommand = Tuple.Create(1, text.Substring(deleteFrom));  
+                 
+                if (!isUndo) {                                 
+                    stackCommands.Push(undoCommand);
+                }
+
+                text = text.Remove(deleteFrom);
+                break;
+            case 3:
+                k = Convert.ToInt32(command.Item2);
+                Console.WriteLine(text[k - 1]);
+                break;
+            case 4:
+                undoCommand = stackCommands.Pop();
+                CheckCommand(ref stackCommands, undoCommand, ref text, true);
+                break;
+            default:
+                break;
+        }
+        
+    }
+    
+    static void CheckCommandList(List<Tuple<int, string>> input) {
+        string text = "";
+        Stack<Tuple<int, string>> stackCommands = new Stack<Tuple<int, string>>();
+        
+        foreach (Tuple<int, string> command in input)
+        {
+            CheckCommand(ref stackCommands, command, ref text);
+            
+        }        
+    }
+    
+    static void Main(String[] args) {
+        int t = Convert.ToInt32(Console.ReadLine().Trim());
+        List<Tuple<int, string>> input = new List<Tuple<int, string>>();
+        
+        for (int tItr = 0; tItr < t; tItr++)
+        {
+            string[] firstMultipleInput = Console.ReadLine().TrimEnd().Split(' ');
+            
+            int q = Convert.ToInt32(firstMultipleInput[0]);
+            string attrs = q == 4 ? "" : firstMultipleInput[1];
+            input.Add(Tuple.Create(q, attrs));
+        }
+        
+        CheckCommandList(input);
+    }
+}
+
 */
